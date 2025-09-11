783f5d4f6084e6497a2dc56fb2f178677f3033a0
Walteann <walteann3@gmail.com>
2025-09-11 19:09:06 -0300

    feat: validacao do formulario sign up

--- src/app/components/ui/input-checkbox/InputCheckbox.tsx
+++ src/app/components/ui/input-checkbox/InputCheckbox.tsx
@@ -1,45 +1,62 @@
 "use client";
 
-import { useState } from "react";
+import { InputHTMLAttributes, useState } from "react";
 
-interface InputCheckboxProps {
-  label: string;
+interface InputCheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+	label: string;
+	error?: string | undefined;
 }
 
-export default function InputCheckbox({ label }: InputCheckboxProps) {
-  const [checked, setChecked] = useState(false);
+export default function InputCheckbox({
+	label,
+	error,
+	...rest
+}: InputCheckboxProps) {
+	const [checked, setChecked] = useState(false);
 
-  return (
-    <label className="flex items-center gap-[16px] cursor-pointer select-none">
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={() => setChecked(!checked)}
-        className="peer hidden"
-      />
-      {/* Quadrado customizado */}
-      <span
-        className={`
+	return (
+		<div>
+			<label className="flex items-center gap-[16px] cursor-pointer select-none">
+				<input
+					type="checkbox"
+					checked={checked}
+					onChange={() => setChecked(!checked)}
+					className="peer hidden"
+					{...rest}
+				/>
+				{/* Quadrado customizado */}
+				<span
+					className={`
           w-[24px] h-[24px] flex items-center justify-center rounded border-2 w-max-fit
           ${checked ? "bg-success border-success" : "border-success"}
           peer-focus:ring-2 peer-focus:ring-green-300
-          transition-colors
+          transition-colors ${error ? "!border-error" : "border-success"}
         `}
-      >
-        {checked && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-[24px] h-[24px]  text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={3}
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-          </svg>
-        )}
-      </span>
-      <span className="w-[250px] md:w-[400px] text-md font-regular">{label}</span>
-    </label>
-  );
+				>
+					{checked && (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="w-[24px] h-[24px]  text-white"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							strokeWidth={3}
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M5 13l4 4L19 7"
+							/>
+						</svg>
+					)}
+				</span>
+				<span className="w-[250px] md:w-[400px] text-md font-regular">
+					{label}
+				</span>
+			</label>
+			{error && (
+				<span className="text-error text-sm pt-[8px]">{error}</span>
+			)}
+		</div>
+	);
 }
--- src/app/components/ui/modal/SignUp.tsx
+++ src/app/components/ui/modal/SignUp.tsx
@@ -1,33 +1,106 @@
-import Image from "next/image"
-import InputText from "../input-text/InputText"
-import Button from "../button/Button"
-import InputCheckbox from "../input-checkbox/InputCheckbox"
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export const SignUp = () => { 
-    return (
-        <form className="flex flex-col gap-3 relative py-[26px] overflow-y-auto scroll-thin max-h-[85vh] md:max-h-none md:w-[445px] md:py-[40px] md:items-center md:justify-center lg:md:w-[589px]">
-            <div className="w-full relative w-[294px] h-[217px] md:w-[354.97px] md:h-[261.59px] flex items-center">
-                <Image fill className="object-cover !relative" src="/images/Ilustração_SignUp.svg" alt="ilustracao login" />
-            </div>
-			<h2 className="text-xl font-bold mb-4">Preencha os campos abaixo para criar sua conta corrente!</h2>
+import Image from "next/image";
+import InputText from "../input-text/InputText";
+import Button from "../button/Button";
+import InputCheckbox from "../input-checkbox/InputCheckbox";
 
-            <div className="w-full flex gap-[24px] flex-col">
-                <InputText label="Nome" name="Nome" type="text" placeholder="Digite seu nome completo"/>
+const signUpSchema = z.object({
+	nome: z
+		.string()
+		.min(3, "O nome precisa ter no mínimo 3 caracteres")
+		.nonempty("Nome é obrigatório"),
+	email: z
+		.email("Dado incorreto. Revise e digite novamente.")
+		.nonempty("Email é obrigatório"),
+	senha: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+	aceite: z.boolean().refine((val) => val, {
+		message: "Você precisa aceitar a política de privacidade",
+	}),
+});
 
-                <InputText label="E-mail" name="E-mail" type="email" placeholder="Digite seu nome completo"/>
+type SignUpSchema = z.infer<typeof signUpSchema>;
 
-                <InputText label="Senha" name="Senha" type="password" placeholder="Digite seu nome completo" className="md:w-[280px]"/>
+export const SignUp = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<SignUpSchema>({
+		resolver: zodResolver(signUpSchema),
+	});
 
-            </div>
+	const onSubmit = (data: SignUpSchema) => {
+		console.log("Form enviado:", data);
+	};
 
-            <div className="w-full py-[16px]">
-                <InputCheckbox label="Li e estou ciente quanto às condições de tratamento dos meus dados conforme descrito na Política de Privacidade do banco."/>
-            </div>
+	return (
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className="flex flex-col gap-3 relative py-[26px] overflow-y-auto scroll-thin max-h-[85vh] md:max-h-none md:w-[445px] md:py-[40px] md:items-center md:justify-center lg:md:w-[589px]"
+		>
+			<div className="w-full relative w-[294px] h-[217px] md:w-[354.97px] md:h-[261.59px] flex items-center">
+				<Image
+					fill
+					className="object-cover !relative"
+					src="/images/Ilustração_SignUp.svg"
+					alt="ilustracao login"
+				/>
+			</div>
+			<h2 className="text-xl font-bold mb-4">
+				Preencha os campos abaixo para criar sua conta corrente!
+			</h2>
 
-            <div className="w-full flex justify-center">
-                <Button value="Criar conta" className="max-w-fit !bg-accent hover:!bg-primary md:!max-w-none"/>
-            </div>
+			<div className="w-full flex gap-[24px] flex-col">
+				<div>
+					<InputText
+						{...register("nome")}
+						label="Nome"
+						type="text"
+						placeholder="Digite seu nome completo"
+						error={errors.nome?.message}
+					/>
+				</div>
 
+				<div>
+					<InputText
+						label="E-mail"
+						type="email"
+						placeholder="Digite seu nome completo"
+						{...register("email")}
+						error={errors.email?.message}
+					/>
+				</div>
+
+				<div>
+					<InputText
+						label="Senha"
+						type="password"
+						{...register("senha")}
+						placeholder="Digite seu nome completo"
+						className="md:w-[280px]"
+						error={errors.senha?.message}
+					/>
+				</div>
+			</div>
+
+			<div className="w-full py-[16px]">
+				<InputCheckbox
+					{...register("aceite")}
+					error={errors.aceite?.message}
+					label="Li e estou ciente quanto às condições de tratamento dos meus dados conforme descrito na Política de Privacidade do banco."
+				/>
+			</div>
+
+			<div className="w-full flex justify-center">
+				<Button
+					onClick={handleSubmit(onSubmit)}
+					value="Criar conta"
+					className="max-w-fit !bg-accent hover:!bg-primary md:!max-w-none"
+				/>
+			</div>
 		</form>
-    )
-}
+	);
+};
